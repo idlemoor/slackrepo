96991360bf83129f73cd997a7b00054150856610
David Spencer <baildon.research@googlemail.com>
2013-12-04 08:06:55 +0000

    New functions build_package and arch_unsupported

--- functions.d/newfunctions.sh
+++ functions.d/newfunctions.sh
@@ -4,10 +4,12 @@
 #   list_direct_deps
 #   build_with_deps
 #   build_package
+#   check_package
 #   install_with_deps
 #   install_package
 #   uninstall_with_deps
 #   uninstall_package
+#   arch_unsupported
 #
 # Copyright 2013 David Spencer, Baildon, West Yorkshire, U.K.
 # All rights reserved.
@@ -63,7 +65,103 @@ function build_with_deps
 function build_package
 {
   local prg="$1"
-  :
+  # Returns:
+  # 1 - build failed
+  # 2 - download failed
+  # 3 - checksum failed
+  # 4 - installpkg returned nonzero
+  # 5 - skipped by hint, or unsupported on this arch
+  # 6 - build returned 0 but nothing in $OUTPUT
+
+  local category=$(cd $SBOREPO/*/$prg/..; basename $(pwd))
+  echo_lined "$category/$prg"
+  rm -f $LOGDIR/$prg.log
+
+  # Load up the .info (and BUILD from the SlackBuild)
+  . $SBOREPO/$category/$prg/$prg.info
+  if [ "$PRGNAM" != "$prg" ]; then
+    echo_yellow "WARNING: PRGNAM in $SBOREPO/$category/$prg/$prg.info is '$PRGNAM', not $prg"
+  fi
+  unset BUILD
+  buildassign=$(grep '^BUILD=' $SBOREPO/*/$PRGNAM/$PRGNAM.SlackBuild)
+  eval $buildassign
+  # At this point we have a full set of environment variables for called functions to use:
+  # PRGNAM VERSION SLKARCH BUILD TAG DOWNLOAD* MD5SUM* etc
+
+  arch_unsupported $prg && return 5
+  hint_skipme $prg && return 5
+
+  # Get the source and symlink it into the SlackBuild directory
+  if [ -d $SRCCACHE/$prg ]; then
+    if ! check_src ; then
+      echo "Note: bad checksums in cached source, will download again"
+      download_src
+      case $? in
+        0) check_src || { save_bad_src; itfailed; return 3; } ;;
+        2) rm -rf $SRCCACHE/$prg;  return 5 ;;
+        *) save_bad_src; itfailed; return 2 ;;
+      esac
+    fi
+  else
+    download_src
+    case $? in
+      0) check_src || { save_bad_src; itfailed; return 3; } ;;
+      2) rm -rf $SRCCACHE/$prg;  return 5 ;;
+      *) save_bad_src; itfailed; return 2 ;;
+    esac
+  fi
+  ln -sf -t $SBOREPO/$category/$prg/ $SRCCACHE/$prg/*
+
+  # Get any hints for the build
+  hint_uidgid $prg
+  tempmakeflags="$(hint_makeflags $prg)"
+  [ -n "$tempmakeflags" ] && echo "Hint: $tempmakeflags"
+  options="$(hint_options $prg)"
+  [ -n "$options" ] && echo "Hint: options=\"$options\""
+  BUILDCMD="env $tempmakeflags $options sh ./$prg.SlackBuild"
+  if [ -f $HINTS/$prg.answers ]; then
+    echo "Hint: supplying answers from $HINTS/$prg.answers"
+    BUILDCMD="cat $HINTS/$prg.answers | $BUILDCMD"
+  fi
+
+  # Build it
+  echo "SlackBuilding $prg.SlackBuild ..."
+  export OUTPUT=$OUTREPO/$prg
+  rm -rf $OUTPUT/*
+  mkdir -p $OUTPUT
+  ( cd $SBOREPO/$category/$prg; eval $BUILDCMD ) >>$LOGDIR/$prg.log 2>&1
+  stat=$?
+  if [ $stat != 0 ]; then
+    echo "ERROR: $prg.SlackBuild failed (status $stat)"
+    itfailed
+    return 1
+  fi
+
+  # Make sure we got *something* :-)
+  pkglist=$(ls $OUTPUT/*.t?z 2>/dev/null)
+  if [ -z "$pkglist" ]; then
+    echo "ERROR: no packages found in $OUTPUT"
+    itfailed
+    return 6
+  fi
+
+  # Install the built packages
+  # (this supports multiple output packages because some Slackware SlackBuilds do that)
+  for pkgpath in $pkglist; do
+    check_package $pkgpath
+    echo "Installing $pkgpath ..."
+    installpkg --terse $pkgpath
+    stat=$?
+    if [ $stat != 0 ]; then
+      echo "ERROR: installpkg $pkgpath failed (status $stat)"
+      itfailed
+      return 4
+    fi
+    dotprofilizer $pkgpath
+  done
+
+  itpassed  # \o/
+  return 0
 }
 
 #-------------------------------------------------------------------------------
@@ -97,7 +195,10 @@ function check_package
         echo_yellow "Suffix .$pext is not .t[gblx]z"
       ;;
     esac
-  # Check the package contents [UNIMPLEMENTED]
+  # Check the package contents
+  if tar tf $pkgpath | grep -q -v ^bin -v ^etc -v ^lib -v ^opt -v ^sbin -v ^usr -v ^var; then
+    echo_yellow "WARNING: $pkgpath installs some weird shit"
+  fi
 }
 
 #-------------------------------------------------------------------------------
@@ -173,4 +274,22 @@ function uninstall_package
   # Do this last so it can mend things we broke
   # (e.g. by reinstalling a Slackware package)
   hint_cleanup $prg
+}
+
+#-------------------------------------------------------------------------------
+
+function arch_unsupported
+{
+  local prg="$1"
+  . $SBOREPO/*/$prg/$prg.info
+  case $SLKARCH in
+    i?86) DOWNLIST="$DOWNLOAD" ;;
+  x86_64) DOWNLIST="${DOWNLOAD_x86_64:-$DOWNLOAD}" ;;
+       *) DOWNLIST="$DOWNLOAD" ;;
+  esac
+  if [ "$DOWNLIST" != "UNSUPPORTED" -a "$DOWNLIST" != "UNTESTED" ]; then
+    return 1
+  fi
+  echo_yellow "$prg is $DOWNLIST on $SLKARCH"
+  return 0
 }

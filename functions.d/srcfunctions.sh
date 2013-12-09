0c3a7f03a8922b0c7618e2539d449bc03a1e1a65
David Spencer <baildon.research@googlemail.com>
2013-12-09 17:48:51 +0000

    trim licence headers, add SlackBuild

--- functions.d/srcfunctions.sh
+++ functions.d/srcfunctions.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
+# Copyright 2013 David Spencer, Baildon, West Yorkshire, U.K.
+# All rights reserved.  For licence details, see the file 'LICENCE'.
 #-------------------------------------------------------------------------------
-# srcfunctions.sh - source functions for SBoggit:
+# srcfunctions.sh - source functions for sboggit:
 #   check_src
 #   download_src
 #   save_bad_src
 #   clean_srcdir
-#
-# Copyright 2013 David Spencer, Baildon, West Yorkshire, U.K.
-# All rights reserved.
-#
-# Redistribution and use of this script, with or without modification, is
-# permitted provided that the following conditions are met:
-#
-# 1. Redistributions of this script must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
-#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
-#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #-------------------------------------------------------------------------------
 
 function check_src

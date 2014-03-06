9fb1540b609544d91da31de65e57d1ee13dae81d
David Spencer <baildon.research@googlemail.com>
2014-03-06 00:06:58 +0000

    Add SlackBuild

---
+++ SlackBuild/doinst.sh
@@ -0,0 +1,18 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/slackrepo/repo.default.new
+
+for h in etc/slackrepo/repo*.conf.new etc/slackrepo/hints*/*/*.new; do
+  config $h
+done
